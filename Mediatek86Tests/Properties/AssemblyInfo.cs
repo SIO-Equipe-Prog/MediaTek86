a8de718b3249c857a655052a3ae0694909f9aabf
Jamil18474 <jamil.zahrane@gmail.com>
2022-02-14 20:45:13 +0100

    Ajout test unitaire Abonnement

---
+++ Mediatek86Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mediatek86Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mediatek86Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a0169b5-288b-4dc4-b21a-4db135096812")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mediatek86Tests/metier/AbonnementTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mediatek86.metier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediatek86.metier.Tests
+{
+    [TestClass()]
+    public class AbonnementTests
+    {
+
+        [TestMethod()]
+        public void ParutionDansAbonnementTest()
+        {
+         DateTime dateCommande = DateTime.Parse("2022-02-14");
+         DateTime dateAchat = DateTime.Parse("2022-02-17");
+         DateTime dateFinAbonnement = DateTime.Parse("2022-02-20");
+         string id = "10001";
+         double montant = 50;
+         string idRevue = "10001";
+         Abonnement abonnement = new Abonnement(id, dateFinAbonnement, dateCommande, montant, idRevue);
+         Assert.AreEqual(true, abonnement.ParutionDansAbonnement(dateCommande, dateAchat, dateFinAbonnement), "devrait réussir : date de parution entre la date de commande et la date de fin d'abonnement");
+        }
+    }
+}
