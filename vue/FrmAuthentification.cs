f55879cbd67be6b7f9c8386aedec33cb7822e88b
Jamil18474 <jamil.zahrane@gmail.com>
2022-03-13 23:41:23 +0100

    Ajout d'une première fenêtre d'authentification et implémentation d'un contrôle d'accès pour l'administrateur et pour un employé du service administratif.

---
+++ vue/FrmAuthentification.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mediatek86.controleur;
+
+namespace Mediatek86.vue
+{
+    public partial class FrmAuthentification : Form
+    {
+        /// <summary>
+        /// instance du controleur
+        /// </summary>
+        private Controle controle;
+
+        public FrmAuthentification(Controle controle)
+        {
+            InitializeComponent();
+            this.controle = controle;
+        }
+
+        /// <summary>
+        /// Demande au controleur de contrôler l'authentification de l'utilisateur
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!txtLogin.Text.Equals("") && !txtPwd.Text.Equals(""))
+            {
+                if (!controle.AdminAuthentification(txtLogin.Text, txtPwd.Text))
+                {
+                    MessageBox.Show("Authentification incorrecte ou vous n'êtes pas admin", "Alerte");
+                    txtLogin.Text = "";
+                    txtPwd.Text = "";
+                    txtLogin.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tous les champs doivent être remplis", "Information");
+            }
+        }
+    }
+}
